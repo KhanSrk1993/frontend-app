e7ae328ccaebbe2bfb1ae1be39d863970cd77f3d
Shahrukh khan <shahrukhkhan9912@gmail.com>
2023-12-09 12:47:05 +0530

    frontend changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState, useEffect } from "react";
+import "./App.css";
+import WaterFallImg from "./Img/WaterFall.jpg";
+// import { uploadFile } from "./services/api.js";
 
 function App() {
+  const [file, setFile] = useState("");
+  const [result, setResult] = useState('');
+
+  const fileInputRef = useRef();
+
+  useEffect(() => {
+    const getImage = async () => {
+      if (file) {
+        const data = new FormData();
+        data.append("name", file.name);
+        data.append("file", file);
+
+        let response = await uploadFile(data);
+        setResult(response.path);
+        console.log(response);
+      }
+    }
+    getImage();
+  }, [file]);
+
+  const onUploadClick = () => {
+    fileInputRef.current.click();
+  };
+
+  console.log(file); 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img src={WaterFallImg} alt="banner" />
+      <div className="wrapper">
+        <h1> File Sharing App </h1>
+        <h3>Upload and share the Download link.</h3>
+        <button onClick={() => onUploadClick()}>Upload</button>
+        <input
+          type="file"
+          ref={fileInputRef}
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+        <a href={result} target='_blank' > {result} </a>
+      </div>
     </div>
   );
 }
